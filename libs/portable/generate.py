6920e6de51abecae9da9275bea5da5ec56d579fa
minkik <minkik@zeronsoftn.com>
2024-08-09 10:28:11 +0900

    temp

--- libs/portable/generate.py
+++ libs/portable/generate.py
@@ -9,7 +9,52 @@ length_count = 4
 # encoding
 encoding = 'utf-8'
 
+def generate_md5_table(folder: str) -> dict:
+    res: dict = dict()
+    curdir = os.curdir
+    os.chdir(folder)
+    try:
+        for root, _, files in os.walk('.'):
+            for f in files:
+                md5_generator = md5()
+                full_path = os.path.join(root, f)
+                print(f"processing {full_path}...")
 
+                # 파일을 여는 부분에 with 사용
+                with open(full_path, "rb") as file:
+                    content = file.read()
+
+                content_compressed = brotli.compress(content, quality=compress_level)
+                md5_generator.update(content)
+                md5_code = md5_generator.hexdigest().encode(encoding=encoding)
+                res[full_path] = (content_compressed, md5_code)
+    finally:
+        os.chdir(curdir)
+
+    return res
+
+def write_metadata(md5_table: dict, output_folder: str, exe: str):
+    output_path = os.path.join(output_folder, "data.bin")
+    with open(output_path, "wb") as f:
+        f.write("rustdesk".encode(encoding=encoding))
+        for path in md5_table.keys():
+            (compressed_data, md5_code) = md5_table[path]
+            data_length = len(compressed_data)
+            path = path.encode(encoding=encoding)
+            # path length & path
+            f.write((len(path)).to_bytes(length=length_count, byteorder='big'))
+            f.write(path)
+            # data length & compressed data
+            f.write(data_length.to_bytes(
+                length=length_count, byteorder='big'))
+            f.write(compressed_data)
+            # md5 code
+            f.write(md5_code)
+        # end
+        f.write("rustdesk".encode(encoding=encoding))
+        # executable
+        f.write(exe.encode(encoding='utf-8'))
+    print(f"metadata had written to {output_path}")
 
 
 def build_portable(output_folder: str):
@@ -38,4 +83,6 @@ if __name__ == '__main__':
         exit(-1)
     exe = '.' + exe[len(os.path.abspath(folder)):]
     print("executable path: " + exe)
+    md5_table = generate_md5_table(folder)
+    write_metadata(md5_table, output_folder, exe)
     build_portable(output_folder)
